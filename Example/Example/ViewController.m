8433525fe82d76c0b7c2d0f2dbf8cb49e0852660
Evgeniy Yurtaev <evgeny.yurtaev@e-legion.com>
2015-10-11 22:46:56 +0300

    Allow enter only numbers into first field

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -7,21 +7,22 @@
 //
 
 #import "ViewController.h"
+#import "EYMaskedTextField.h"
 
-@interface ViewController ()
+@interface ViewController () <EYMaskedTextFieldDelegate>
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)textField:(UITextField *)textField shouldChangeUnformattedText:(NSString *)unformattedText inRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSString *targetText = [unformattedText stringByReplacingCharactersInRange:range withString:string];
+    NSCharacterSet *notNumberCharacterSet = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789"] invertedSet];
+    NSRange notNumberCharacterRange = [targetText rangeOfCharacterFromSet:notNumberCharacterSet options:0];
+    BOOL isNumber = (notNumberCharacterRange.length == 0);
+    
+    return isNumber;
 }
 
 @end
